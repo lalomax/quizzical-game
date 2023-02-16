c060d4b5cabb674236aa10539e9da7cb458f4dee
lalomax <lalomax365@gmail.com>
2023-02-16 18:11:47 -0400

    Another version

--- src/App.jsx
+++ src/App.jsx
@@ -3,52 +3,77 @@ import Button from "./components/Button";
 import { nanoid } from "nanoid";
 import Confetti from "react-confetti";
 import "./App.css";
-import Swal from "sweetalert2";
 import usefetch from "./usefetch";
 
 function App() {
   const [items, setItems] = useState([]);
   const [isOver, setIsOver] = useState(false);
 
-  const {} = usefetch()
+  const { data, isLoaded, error, refetch } = usefetch(
+    "https://opentdb.com/api.php?amount=5"
+  );
+
+  function shuffle(array) {
+    let currentIndex = array.length,
+      randomIndex;
+
+    // While there remain elements to shuffle.
+    while (currentIndex != 0) {
+      // Pick a remaining element.
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex--;
+
+      // And swap it with the current element.
+      [array[currentIndex], array[randomIndex]] = [
+        array[randomIndex],
+        array[currentIndex],
+      ];
+    }
+
+    return array;
+  }
+
+  // if (isLoaded) {
+  //   console.log(data.data.results)
+
+  // } else {
+  //   return <div className="">Loading</div>
+  // }
 
   useEffect(() => {
-    fetch("https://opentdb.com/api.php?amount=5")
-      .then((res) => res.json())
-      .then((data) => {
-        setItems(
-          data.results.map((el) => {
-            return {
-              question: el.question,
-              point: 0,
-              answers: [
-                {
-                  answer: el.correct_answer,
-                  isCorrect: true,
+    if (isLoaded) {
+      setItems(
+        data.data.results.map((el) => {
+          return {
+            question: el.question,
+            point: 0,
+            answers: shuffle([
+              {
+                answer: el.correct_answer,
+                isCorrect: true,
+                isSelected: false,
+                id: nanoid(),
+              },
+            ].concat(
+              el.incorrect_answers.map((el) => {
+                return {
+                  answer: el,
+                  isCorrect: false,
                   isSelected: false,
                   id: nanoid(),
-                },
-              ].concat(
-                el.incorrect_answers.map((el) => {
-                  return {
-                    answer: el,
-                    isCorrect: false,
-                    isSelected: false,
-                    id: nanoid(),
-                  };
-                })
-              ),
-            };
-          })
-        );
-      });
-
-      // handleGame()  
-  }, []);
+                };
+              })
+            ))
+          };
+        })
+      );
+    }
+    // handleGame()
+  }, [isLoaded]);
 
   useEffect(() => {
-    setIsOver(items.every((item) => item.point))
-  }, [items])
+    setIsOver(items.every((item) => item.point));
+  }, [items]);
 
   function holdSelection(id, i) {
     setItems((oldItems) => {
@@ -73,10 +98,10 @@ function App() {
 
   let itemsArray = items.map((el, i) => {
     const styles = {
-      backgroundColor: el.point === 1 ? "dodgerblue" : "black",
+      backgroundColor: el.point === 1 ? "#3b9660" : "black",
     };
     return (
-      <div key={i} point={items.point} style={styles}>
+      <div className="row" key={i} point={items.point} style={styles}>
         <div dangerouslySetInnerHTML={{ __html: el.question }} />
         {el.answers.map((item) => {
           return (
@@ -88,24 +113,22 @@ function App() {
             />
           );
         })}
-        {i + 1 < items.length && <hr />}
+        {/* {i + 1 < items.length && <hr />} */}
       </div>
     );
   });
 
-  function handleGame() {
-  //  const countAnswers = items.reduce((ac, cur) => ac + cur.point, 0 )
-  //  if (countAnswers === items.length) setIsOver(true)
-  // if (!isOver) 
-   
-  }
+  // function handleGame() {
+  //   setIsOver(false)
+  //   refetch
+  // }
 
   return (
     <div className="App">
       {isOver && <Confetti />}
       {itemsArray}
-      <button className="button-check" onClick={handleGame}>
-        {isOver ? "New Game" : "Check results"}
+      <button className="button-check" onClick={refetch}>
+        New Game
       </button>
     </div>
   );
--- src/usefetch.js
+++ src/usefetch.js
@@ -1,19 +1,28 @@
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
 import axios from "axios"
 
 function usefetch(url) {
-  const [data, setData] = useEffect(null)
-  const [loading, setLoading] = useEffect(false)
-  const [error, setError] = useEffect(null)
+  const [data, setData] = useState(null)
+  const [isLoaded, setIsLoaded] = useState(false)
+  const [error, setError] = useState(null)
   useEffect(() => {
-    setLoading(true)
     axios
       .get(url)
-      .then(response => setData(response))
+      .then(response => { setData(response); setIsLoaded(true) })
       .catch(err => setError(err))
-      .finally(() => setLoading(false))
+      
   }, [url])
-return {data, loading, error}
+
+  const refetch = () => {
+    setIsLoaded(false);
+    axios
+    .get(url)
+    .then(response => { setData(response); setIsLoaded(true) })
+    .catch(err => setError(err))
+  };
+
+
+  return { data, isLoaded, error, refetch }
 }
 
 export default usefetch
